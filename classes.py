d837e526dff1ae2f7f200aff9888140598e868ea
phil-qa <104486392+phil-qa@users.noreply.github.com>
2022-07-27 08:20:54 +0100

    Adding more formatting examples

--- classes.py
+++ classes.py
@@ -2,14 +2,18 @@ class Car():
     def __init__(self, registration):
         self.speed=0
         self.reg=registration
+        self._accelleration_rate = 100
 
-    def accelerate(self):
-        self.speed += 5
+    def accelerate(self, accelerate_by):
+        self._increase_speed(accelerate_by)
+
+    def _increase_speed(self, value):
+        self.speed += self._accelleration_rate * value
 
 
 
 car = Car("VIP 100")
 print(car.reg)
 print(car.speed)
-car.accelerate()
+car.accelerate(10)
 print(car.speed)
--- stringHandling.py
+++ stringHandling.py
@@ -16,3 +16,45 @@ print("Printing ", "some", "things", "To console", "with", "sep=.", sep='.')
 # using end parameter print two separate things on one line
 print("Printing some things with the ", end='')
 print("end=''")
+
+'''
+Using .format to apply formatting 
+'''
+#Passing positional values to the string
+person = {"name": "Phil Bridge", "job" : "Development Manager", "company" : "QA"}
+
+print('my name is {0} I am a {1} for {2}'.format(person["name"], person["job"], person["company"]))
+#mixing it up
+print('my name is {1} I am a {2} for {0}'.format(person["name"], person["job"], person["company"]))
+#using F string
+print(f'my name is {person["name"]} I am a {person["job"]} for {person["company"]}')
+
+#Treating bits of data at types in a string, the place holders here are positional and one
+#represents the data as repr and the other as a str
+print("repr() shows quotes: {!r}; str() doesn't: {!s}".format('test1', 'test2'))
+
+#Formatting with justification and spaces
+#This aligns left and sets 30 character spaces width
+print('{:<30}'.format('left aligned'))
+#is the width fixed
+print('{:<1}'.format('left aligned, is the width fixed'))
+
+#right aligned
+print('{:>30}'.format('right aligned'))
+
+#center formatted
+print('{:^30}'.format('centered'))
+
+#center formatted with padding character
+print('{:*^30}'.format('centered'))
+
+#Printing signs with numeric data
+
+#always show a sign
+print('all signs shown \n{:+f}; {:+f}'.format(3.14, -3.14) )
+
+#show a space for a + and - for -
+print('show a - sign when negative but only a space for positive \n{: f}; {: f}'.format(3.14, -3.14))
+
+#only show - dont do anything for +
+print('Show the - but do nothing with the positive number\n{:-f}; {:-f}'.format(3.14, -3.14))
